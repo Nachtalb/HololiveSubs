94a8067431588b4dc2a00bab0fac97eb7e20ff5c
Nachtalb <info@nachtalb.io>
2022-06-25 00:19:35 +0200

    Minimize shading code

--- www/resources/js/main.js
+++ www/resources/js/main.js
@@ -58,8 +58,8 @@ Vue.component('member-card', {
     },
     scheduledTitle: function () {
       if (this.member.next_live > 0) {
-          const date = new Date(this.member.next_live * 1000)
-          return "Live " + moment(date).fromNow()
+        const date = new Date(this.member.next_live * 1000)
+        return "Live " + moment(date).fromNow()
       }
     },
     mainLink: function () {
@@ -74,7 +74,7 @@ Vue.component('member-card', {
       }
     },
     liStyle: function () {
-      let style = {backgroundImage: `linear-gradient(180deg, ${this.member.bg} 0%, ${this.pSBC(-.9, this.member.bg)} 100%)`}
+      let style = {backgroundImage: `linear-gradient(180deg, ${this.member.bg} 0%, ${this.colourShade(-.9, this.member.bg)} 100%)`}
       if (this.member.background_image && !app.settings.simpleBackgrounds.value) {
         style.backgroundImage = `url("${this.member.background_image}"), ${style.backgroundImage}`
       }
@@ -89,34 +89,12 @@ Vue.component('member-card', {
     }
   },
   methods: {
-
     // https://stackoverflow.com/a/13542669/5699307
-    pSBC(p, c0, c1, l) {
-      let r, g, b, P, f, t, h, i = parseInt, m = Math.round, a = typeof (c1) == "string";
-      if (typeof (p) != "number" || p < -1 || p > 1 || typeof (c0) != "string" || (c0[0] != 'r' && c0[0] != '#') || (c1 && !a)) return null;
-      if (!this.pSBCr) this.pSBCr = (d) => {
-        let n = d.length, x = {};
-        if (n > 9) {
-          [r, g, b, a] = d = d.split(","), n = d.length;
-          if (n < 3 || n > 4) return null;
-          x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4)), x.g = i(g), x.b = i(b), x.a = a ? parseFloat(a) : -1
-        } else {
-          if (n == 8 || n == 6 || n < 4) return null;
-          if (n < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : "");
-          d = i(d.slice(1), 16);
-          if (n == 9 || n == 5) x.r = d >> 24 & 255, x.g = d >> 16 & 255, x.b = d >> 8 & 255, x.a = m((d & 255) / 0.255) / 1000;
-          else x.r = d >> 16, x.g = d >> 8 & 255, x.b = d & 255, x.a = -1
-        } return x
-      };
-      h = c0.length > 9, h = a ? c1.length > 9 ? true : c1 == "c" ? !h : false : h, f = this.pSBCr(c0), P = p < 0, t = c1 && c1 != "c" ? this.pSBCr(c1) : P ? {r: 0, g: 0, b: 0, a: -1} : {r: 255, g: 255, b: 255, a: -1}, p = P ? p * -1 : p, P = 1 - p;
-      if (!f || !t) return null;
-      if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
-      else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
-      a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
-      if (h) return "rgb" + (f ? "a(" : "(") + r + "," + g + "," + b + (f ? "," + m(a * 1000) / 1000 : "") + ")";
-      else return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2)
+    // Takes a 6 char hex, shades it and returns a rgb(...) value
+    colourShade(p, c) {
+      var m = Math.round, c = parseInt(c.slice(1), 16), r = c >> 16, g = c >> 8 & 255, b = c & 255, P = p < 0, t = P ? 0 : p * 255 ** 2, P = P ? 1 + p : 1 - p;
+      return "rgb" + "(" + m((P * r ** 2 + t) ** 0.5) + "," + m((P * g ** 2 + t) ** 0.5) + "," + m((P * b ** 2 + t) ** 0.5) + ")";
     },
-
     nFormatter(num, digits) {
       if (num === undefined) return;
       var si = [
